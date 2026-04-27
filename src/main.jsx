4a6e9afd5473dd76f1b793216502af6ba08a4b63
sanjeevdg <sanjeev.dasgupta@gmail.com>
2026-04-27 21:32:12 +0530

    apr27-2026-2132hrs

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { WatchlistProvider } from "./context/WatchlistContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <WatchlistProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </WatchlistProvider>
   </React.StrictMode>
 )
